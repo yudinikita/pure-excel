1f353fb2e6a04c79a52de90e2f41d7c12003d5a3
nBlackNinja <nikitayudin782@gmail.com>
2021-08-24 18:23:18 +0300

    Framework start structure

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,21 @@
+export class Excel {
+  constructor(selector, options) {
+    this.$el = document.querySelector(selector);
+    this.components = options.components || [];
+  }
+
+  getRoot() {
+    const $root = document.createElement('div');
+
+    this.components.forEach((Component) => {
+      const component = new Component();
+      $root.insertAdjacentElement('beforeend', component.toHTML());
+    });
+
+    return $root;
+  }
+
+  render() {
+    this.$el.append(this.getRoot());
+  }
+}
